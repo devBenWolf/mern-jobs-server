95c67782d5a8c76d880afe4724b9b91a4d121a54
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-09-16 17:24:48 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express"
+const app = express()
+import dotenv from "dotenv"
+dotenv.config()
+
+// Middleware imports
+import notFound from "./middleware/not-found.js"
+import errorHandler from "./middleware/error-handler.js"
+
+// router import
+import authRouter from "./routes/authRoutes.js"
+import jobsRouter from "./routes/jobsRoutes.js"
+
+// DB Connection import 
+import connectDB from "./db/connect.js"
+
+app.use(express.json())
+
+app.get(`/`, (req, res) => {
+    throw new Error(`Error`)
+    res.send(`Hello!`)
+})
+
+// routers
+app.use(`/api/v1/auth`, authRouter)
+app.use(`/api/v1/jobs`, jobsRouter)
+
+// middleware
+app.use(notFound)
+app.use(errorHandler)
+
+const port  = process.env.PORT || 5000
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server running on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+
+
+console.log(`server is running hello`)
