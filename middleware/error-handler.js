0eac370aa1e93e0fe536651b3008b13047dc7c10
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-09-20 17:58:58 -0400

    Update error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,20 @@
+import { StatusCodes } from "http-status-codes"
+
 const errorHandler = (err, req, res, next) => {
     console.log(err)
-    res.status(500).json({msg: `There was an error`})
+    const defaultError = {
+        statusCode: err.statusCode ? err.statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        message: err.message ? err.message : "Something went wrong, please try again later"
+    }
+    if (err.name === "ValidationError") {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.message = Object.values(err.errors).map((item) => item.message).join(",")
+    }
+    if (err.code && err.code === 11000) {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.message = `${Object.keys(err.keyPattern)} is already in use.`
+    }
+    res.status(defaultError.statusCode).json({msg: defaultError.message})
 }
 
 export default errorHandler
