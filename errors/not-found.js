0a013432c154c4afe1536e31e22405ed1529fc2f
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-09-20 17:58:52 -0400

    Update not-found.js

---
+++ errors/not-found.js
@@ -0,0 +1,12 @@
+import CustomAPIError from "./custom-api.js";
+import { StatusCodes } from "http-status-codes";
+
+class NotFoundError extends CustomAPIError {
+    constructor(message) {
+        super(message)
+
+        this.statusCodes = StatusCodes.NOT_FOUND
+    }
+}
+
+export default NotFoundError
