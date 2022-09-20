7e3038f8f12e546468a472064545970f512ec5f1
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-09-20 17:58:35 -0400

    Update authController.js
    removed try/catch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,33 @@
 import User from "../models/User.js"
+import {StatusCodes} from "http-status-codes"
+import {BadRequestError} from "../errors/index.js"
 
-const register = async(req, res) => {
-    try {
-        const user = await User.create(req.body)     
-        res.status(201).json({user})
-    } catch (error) {
-        res.status(500).json({msg: "There was an error"})
-        console.log(error)
+const register = async(req, res, next) => {
+    const {name, email, password} = req.body
+    if (!name || !email || !password) {
+        throw new BadRequestError(`Please provide all required values.`)
     }
+    
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        throw new BadRequestError(`Email already in use`)
+    }
+
+    const user = await User.create({name, email, password})
+    const token = user.createJWT()
+
+    // Cannot restrict what is sent back in response due to User.create(). This is a hardcoded workaround to receive only
+    // email, name, lastName, location and token:
+    res.status(StatusCodes.CREATED).json({
+        user: {
+            email: user.email,
+            name: user.name,
+            lastName: user.lastName,
+            location: user.location
+        },
+        token
+    })
+
 }
 
 const login = async(req, res) => {
